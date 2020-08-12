3a4e92951266c035215fb833483f8f9a4f232c4b
mazrins <mazrins@outlook.com>
2020-08-12 15:41:21 +0200

    adding firebase configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjYbJ2PYzpqI9kjcUkM3Fn_zUJINImJCw",
+    authDomain: "web-shop-2e344.firebaseapp.com",
+    databaseURL: "https://web-shop-2e344.firebaseio.com",
+    projectId: "web-shop-2e344",
+    storageBucket: "web-shop-2e344.appspot.com",
+    messagingSenderId: "1089283167305",
+    appId: "1:1089283167305:web:2c9a27d3e742ee422f1b98",
+    measurementId: "G-JPRPSS1QVW"
+  }
 };
