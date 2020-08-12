a57e285c398e09301071a807ddc1d4406d6d2696
mazrins <mazrins@outlook.com>
2020-08-12 17:18:25 +0200

    added auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductsListComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'admin', component: AdminDashboardComponent }
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent }
 ];
 
 @NgModule({
